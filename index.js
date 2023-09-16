ce4c7c3bff17c0b85832fa7c1687ba1cc8859431
Moh7ven <Mohamedsangare0107@gmail.com>
2023-09-16 23:11:44 +0000

    new

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import prompt from "prompt-sync";
+import { soustraction, multiplication } from "./tp/tp1.js";
+
+//demande de tapper le premier nombre
+let nombre1 = prompt()("taper le premier: ");
+//demande de tapper le second nombre
+let nombre2 = prompt()("taper le second: ");
+//l'appel de fonction multiplication
+let produit = multiplication(nombre1, nombre2);
+//l'appel de fonction soustraction
+let difference = soustraction(nombre1, nombre2);
+
+//imprimer les resultats dans le terminal
+console.log("le produit : ", produit);
+console.log("la difference: ", difference);
---
+++ tp/tp1.js
@@ -0,0 +1,12 @@
+//Fonction pour la soustraction
+const soustraction = (nb1, nb2)=>{
+    return nb1 - nb2;
+ }
+
+ //Fonction la multiplication
+ const multiplication = (nb3, nb4)=> {
+    return nb3 * nb4;
+ }
+
+ export { soustraction, multiplication };
+ 
